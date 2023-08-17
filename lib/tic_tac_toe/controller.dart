1d5c11b02d690b3af8b62ff37175d25dbe93497a
milanmaniya <milanmaniya981@gmail.com>
2023-08-17 16:09:24 +0530

    done

--- lib/tic_tac_toe/controller.dart
+++ lib/tic_tac_toe/controller.dart
@@ -1,3 +1,72 @@
 import 'package:get/get.dart';
 
-class TicTacToeController extends GetxController {}
+class TicTacToeController extends GetxController {
+  RxString player1 = 'O turn'.obs;
+  RxString player2 = 'X turn'.obs;
+  RxBool isActivated = false.obs;
+  RxInt fillBox = 0.obs, countO = 0.obs, countX = 0.obs, countDraw = 0.obs;
+  List<RxString> boxValue = List.generate(9, (index) => ''.obs);
+
+  void playerSwap(int index, context) {
+    if (boxValue[index].value == '') {
+      if (isActivated.value == true) {
+        boxValue[index].value = 'O';
+        fillBox.value++;
+        isActivated.value = false;
+      } else {
+        boxValue[index].value = 'X';
+        fillBox.value++;
+        isActivated.value = true;
+      }
+    }
+  }
+
+  void winnerState(context) {
+    if (boxValue[0].value == boxValue[1].value &&
+            boxValue[1].value == boxValue[2].value &&
+            boxValue[0].value.isNotEmpty ||
+        boxValue[0].value == boxValue[3].value &&
+            boxValue[3].value == boxValue[6].value &&
+            boxValue[0].value.isNotEmpty ||
+        boxValue[2].value == boxValue[5].value &&
+            boxValue[5].value == boxValue[8].value &&
+            boxValue[2].value.isNotEmpty ||
+        boxValue[6].value == boxValue[7].value &&
+            boxValue[7].value == boxValue[8].value &&
+            boxValue[6].value.isNotEmpty ||
+        boxValue[3].value == boxValue[4].value &&
+            boxValue[4].value == boxValue[5].value &&
+            boxValue[3].value.isNotEmpty ||
+        boxValue[1].value == boxValue[4].value &&
+            boxValue[4].value == boxValue[7].value &&
+            boxValue[1].value.isNotEmpty ||
+        boxValue[2].value == boxValue[4].value &&
+            boxValue[4].value == boxValue[6].value &&
+            boxValue[2].value.isNotEmpty ||
+        boxValue[0].value == boxValue[4].value &&
+            boxValue[4].value == boxValue[8].value &&
+            boxValue[0].value.isNotEmpty) {
+      winnerCount(context);
+      clearControl();
+    } else if (fillBox.value == 9) {
+      Get.snackbar('Player Winner', 'Match Try Please Try Again!....');
+      countDraw.value++;
+      clearControl();
+    }
+  }
+
+  void winnerCount(context) {
+    if (isActivated.value) {
+      countX.value++;
+      Get.snackbar('Winner Name', 'Player X Winner');
+    } else {
+      countO.value++;
+      Get.snackbar('Player Winner', 'Player O Winner');
+    }
+  }
+
+  void clearControl() {
+    boxValue = List.generate(9, (index) => ''.obs);
+    fillBox.value = 0;
+  }
+}
--- lib/tic_tac_toe/tic_tac_toe_ui.dart
+++ lib/tic_tac_toe/tic_tac_toe_ui.dart
@@ -1,10 +1,201 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getxdemo/tic_tac_toe/controller.dart';
 
 class TicTacToeUi extends StatelessWidget {
   const TicTacToeUi({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    TicTacToeController ticTacToeController = Get.find();
+
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            'Tic Tac Toe',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Obx(
+                () => Column(
+                  children: [
+                    const Text(
+                      'Player X',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      ticTacToeController.countX.value.toString(),
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Obx(
+                () => Column(
+                  children: [
+                    const Text(
+                      'Player O',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      ticTacToeController.countO.value.toString(),
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Obx(
+                () => Column(
+                  children: [
+                    const Text(
+                      'Draw',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      ticTacToeController.countDraw.value.toString(),
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+          GridView.count(
+            shrinkWrap: true,
+            crossAxisCount: 3,
+            crossAxisSpacing: 5,
+            mainAxisSpacing: 5,
+            children: List.generate(
+              9,
+              (index) => GestureDetector(
+                onTap: () {
+                  ticTacToeController.playerSwap(index, context);
+                  log(ticTacToeController.boxValue.toString());
+                },
+                child: Card(
+                  elevation: 14,
+                  color: Colors.grey.shade400,
+                  margin: const EdgeInsets.all(3),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    side: const BorderSide(
+                      color: Colors.black26,
+                    ),
+                  ),
+                  child: Center(
+                    child: Obx(
+                      () => Text(
+                        ticTacToeController.boxValue[index].value,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 60,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Obx(
+            () => Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  style: ButtonStyle(
+                    elevation: MaterialStateProperty.all(7),
+                    minimumSize: MaterialStateProperty.all(const Size(150, 50)),
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.grey.shade300),
+                  ),
+                  onPressed: () {
+                    ticTacToeController.clearControl();
+                  },
+                  child: const Text(
+                    'Reset',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  ticTacToeController.isActivated.value
+                      ? ticTacToeController.player1.string
+                      : ticTacToeController.player2.string,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    elevation: MaterialStateProperty.all(7),
+                    minimumSize: MaterialStateProperty.all(const Size(150, 50)),
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.grey.shade300),
+                  ),
+                  onPressed: () {
+                    ticTacToeController.clearControl();
+                  },
+                  child: const Text(
+                    'Restart',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
